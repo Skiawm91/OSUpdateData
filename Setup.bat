0d1534e4a29be8795fadb612b9fd314fe9a9cc2b
Swarmed2674 <96339124+Swarmed2674@users.noreply.github.com>
2023-02-13 21:42:24 +0800

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -5,14 +5,13 @@ cls
 echo Starting Update...
 copy /Y *.* C:\SakuraPC\Systems\GPT\OneOS\Storage\OneOS\System32\
 copy /Y ver.bat C:\SakuraPC\Systems\GPT\OneOS\Storage\etc
-copy /Y infokey.bat C:\SakuraPC\Systems\GPT\OneOS\Storage\etc
 cd ..
 cd ..
 del NowUpdate.tmp
 echo.
 pause >nul
 del ver.bat
-del infokey.bat
+del setup.bat
 start Kernel32.bat
 exit
 
