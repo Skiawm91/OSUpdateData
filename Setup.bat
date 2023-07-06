4811f44ff3dd9c528ea03af3dd6301bb9d1f84e5
Skiawm91 <96339124+Skiawm91@users.noreply.github.com>
2023-07-06 22:44:59 +0800

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -4,7 +4,9 @@ echo.
 cls
 echo Starting Update...
 copy /Y *.* C:\SakuraPC\Systems\GPT\OneOS\Storage\OneOS\System32\
-copy /Y ver.bat C:\SakuraPC\Systems\GPT\OneOS\Storage\etc
+copy /Y ver.bat C:\SakuraPC\Systems\GPT\OneOS\Storage\etc\
+copy /Y sakosv3.bat C:\SakuraPC\Systems\GPT\OneOS\Storage\etc\
+copy /Y seweb.bat C:\SakuraPC\Systems\GPT\OneOS\Storage\ProgramFiles\SExplorer\
 cd ..
 cd ..
 del NowUpdate.tmp
@@ -13,6 +15,8 @@ echo.
 timeout /t 3 /NOBREAK >nul
 del ver.bat
 del setup.bat
+del seweb.bat
+del sakosv3.bat
 start Kernel32.bat
 exit
 
